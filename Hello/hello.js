a55a1fff93be4026d3e64a003dd8b73c8537ebb8
belinguc <belinguier.christopher@gmail.com>
2018-10-12 16:13:26 +0200

    home

--- Hello/hello.js
+++ Hello/hello.js
@@ -3,3 +3,20 @@ var options = {
 };
 
 var userList = new List('users', options); 
+/*
+window.onload=function() {
+  horloge('div_horloge');
+};
+
+function actualiser() {
+var date = new Date();
+var str = date.getHours();
+str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
+el.innerHTML = str;
+}
+	  
+function horloge(el) {
+  if(typeof el=="string") { el = document.getElementById(el); }
+  actualiser();
+  setInterval(actualiser,1000);
+}*/
